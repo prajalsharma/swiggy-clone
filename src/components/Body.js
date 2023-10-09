43205bdca5bda0fe381d3dbc292dc3a4c405e266
Aditya Phasu <adityaphasu919@gmail.com>
2023-10-09 12:39:45 +0530

    Fix Search Functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,78 +4,81 @@ import { useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [listOfRestaurants, setlistofRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    try {
+      const data = await fetch(
+        "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.572588&lng=77.273265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      setlistofRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const [listOfRestaurants, setlistofRestaurant] = useState([]);
-    const [searchText, setSearchText ]= useState ([]);
-    
-    useEffect(() => {
-        fetchData();
-    }, []);
+  //   Function for searching restaurant
 
-   
-    const fetchData = async () => {
-        const data = await fetch (
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.572588&lng=77.273265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        );
-
-        const json = await data.json();
-        console.log(json);
-        setlistofRestaurant(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    };
-
-
-
-
-
-    
-    return listOfRestaurants.length === 0 ? (
-       <Shimmer/> ) :
-    (
-    <div className="body">
-        <div className="filter"> 
-        <div className="search">
-            <input type="text"className="search-box" value={searchText} onChange={(e)=>{
-                setSearchText(e.target.value);
-            }
-
-            }/>
-            <button onClick={()=>{
-             listOfRestaurants.filter((res)=> res?.data?.name.toLowerCase().includes(searchText.toLowerCase()))
-                setlistofRestaurant(filteredRestaurants);
-                console.log(searchText)
-
-            }}
-            className="filter-btn"
-            > Search </button>
-
-        </div>
-        <button className="filter-btn" onClick={()=>{
-            
-            const filteredList =listOfRestaurants?.filter(
-                (res) => res.info.avgRating>4
-            );
-            setlistofRestaurant(filteredList);
-        }}
-        
-        
-        
-        > Top Rated Restaurant</button>
-        </div>
-        <div className="res-container">
-            {listOfRestaurants?.map((restaurant)=> (
-                 
-                 <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-                 ))
-    
-    
-    
-            }
-        </div>
-    </div>
-    
+  const searchRestaurant = () => {
+    const filteredRestaurants = listOfRestaurants?.filter((res) =>
+      res?.info?.name.toLowerCase().includes(searchText.toLowerCase())
     );
-    };
+    setlistofRestaurant(filteredRestaurants);
+    console.log(searchText);
+  };
 
- export default Body ;   
+  //   Function for top rated restaurant
+
+  const topRatedRestaurant = () => {
+    const filteredList = listOfRestaurants?.filter((res) => res.info.avgRating > 4);
+    setlistofRestaurant(filteredList);
+  };
+
+  return listOfRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              searchRestaurant();
+            }}
+            className="filter-btn">
+            Search
+          </button>
+        </div>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            topRatedRestaurant();
+          }}>
+          Top Rated Restaurant
+        </button>
+      </div>
+
+      <div className="res-container">
+        {listOfRestaurants?.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
